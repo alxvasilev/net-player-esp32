37c6198e558819ea8ae6d7346adbd4be68080ee4
Alexander Vassilev <av@mega.co.nz>
2020-06-03 16:44:14 +0300

    Added mutex.hpp

---
+++ main/mutex.hpp
@@ -0,0 +1,34 @@
+#ifndef MUTEX_HPP_INCLUDED
+#define MUTEX_HPP_INCLUDED
+
+#include <freertos/semphr.h>
+
+class Mutex
+{
+    SemaphoreHandle_t mMutex;
+    StaticSemaphore_t mMutexMem;
+public:
+    Mutex() {
+        mMutex = xSemaphoreCreateRecursiveMutexStatic(&mMutexMem);
+    }
+    void lock() { xSemaphoreTakeRecursive(mMutex, portMAX_DELAY); }
+    void unlock() { xSemaphoreGiveRecursive(mMutex); }
+};
+
+class MutexLocker
+{
+    Mutex& mMutex;
+public:
+    MutexLocker(Mutex& aMutex): mMutex(aMutex) { mMutex.lock(); }
+    ~MutexLocker() { mMutex.unlock(); }
+};
+
+class MutexUnlocker
+{
+    Mutex& mMutex;
+public:
+    MutexUnlocker(Mutex& aMutex): mMutex(aMutex) { mMutex.unlock(); }
+    ~MutexUnlocker() { mMutex.lock(); }
+};
+
+#endif
