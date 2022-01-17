29775a1987b026e13a37a079cd943cd1016a5da3
Alexander Vassilev <av@mega.co.nz>
2022-01-17 22:03:33 +0200

    Moved VU display to its own files

--- main/audioPlayer.hpp
+++ main/audioPlayer.hpp
@@ -8,8 +8,7 @@
 #include <st7735.hpp>
 #include "stationList.hpp"
 #include "recorder.hpp"
-#include <limits>
-#include "volume.hpp"
+#include "vuDisplay.hpp"
 
 class DecoderNode;
 class EqualizerNode;
@@ -18,40 +17,6 @@ class ST7735Display;
 namespace nvs {
     class NVSHandle;
 }
-class VuDisplay {
-    enum {
-        kLevelSmoothFactor = 4, kDefPeakHoldMs = 30, kDefPeakDropSpeed = 20,
-        kDefLedWidth = 1, kDefLedHeight = 10, kDefLedSpacing = 1, kDefChanSpacing = 2
-    };
-    enum: uint16_t { kLevelMax = std::numeric_limits<int16_t>::max() };
-    struct ChanCtx
-    {
-        int16_t barY;
-        int16_t avgLevel = 0;
-        int16_t peakLevel = 0;
-        int16_t prevBarLen = 0;
-        uint8_t peakTimer = 0;
-    };
-    ST7735Display& mLcd;
-    ChanCtx mLeftCtx;
-    ChanCtx mRightCtx;
-    int8_t mStepWidth; // mLedWidth + led spacing
-    int8_t mLedWidth;
-    int8_t mLedHeight;
-    int8_t mChanSpacing;
-    int16_t mYellowStartX;
-    int32_t mLevelPerLed;
-    uint8_t mPeakDropTicks;
-    uint8_t mPeakHoldTicks;
-    inline uint16_t ledColor(int16_t ledX, int16_t level);
-    void calculateLevels(ChanCtx& ctx, int16_t level);
-    void drawChannel(ChanCtx& ctx, int16_t level);
-    inline int16_t numLedsForLevel(int16_t level);
-public:
-    VuDisplay(ST7735Display& lcd): mLcd(lcd) {}
-    void init(NvsHandle& nvs);
-    void update(const IAudioVolume::StereoLevels& levels);
-};
 
 class AudioPlayer: public AudioNode::EventHandler
 {
---
+++ main/vuDisplay.hpp
@@ -0,0 +1,45 @@
+#ifndef VU_DISPLAY_H
+#define VU_DISPLAY_H
+#include <stdint.h>
+#include <limits>
+#include "volume.hpp"
+
+class ST7735Display;
+class NvsHandle;
+
+class VuDisplay {
+    enum {
+        kLevelSmoothFactor = 4, kDefPeakHoldMs = 30, kDefPeakDropSpeed = 20,
+        kDefLedWidth = 1, kDefLedHeight = 10, kDefLedSpacing = 1, kDefChanSpacing = 2
+    };
+    enum: uint16_t { kLevelMax = std::numeric_limits<int16_t>::max() };
+    struct ChanCtx
+    {
+        int16_t barY;
+        int16_t avgLevel = 0;
+        int16_t peakLevel = 0;
+        int16_t prevBarLen = 0;
+        uint8_t peakTimer = 0;
+    };
+    ST7735Display& mLcd;
+    ChanCtx mLeftCtx;
+    ChanCtx mRightCtx;
+    int8_t mStepWidth; // mLedWidth + led spacing
+    int8_t mLedWidth;
+    int8_t mLedHeight;
+    int8_t mChanSpacing;
+    int16_t mYellowStartX;
+    int32_t mLevelPerLed;
+    uint8_t mPeakDropTicks;
+    uint8_t mPeakHoldTicks;
+    inline uint16_t ledColor(int16_t ledX, int16_t level);
+    void calculateLevels(ChanCtx& ctx, int16_t level);
+    void drawChannel(ChanCtx& ctx, int16_t level);
+    inline int16_t numLedsForLevel(int16_t level);
+public:
+    VuDisplay(ST7735Display& lcd): mLcd(lcd) {}
+    void init(NvsHandle& nvs);
+    void update(const IAudioVolume::StereoLevels& levels);
+};
+
+#endif
