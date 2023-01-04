9854716aafe6f2507d565a915a397baccb894232
Alexander Vassilev <av@mega.co.nz>
2023-01-04 17:00:47 +0200

    Changed partition table to increase app size due to smaller recovery image

--- esplink-flash.sh
+++ esplink-flash.sh
@@ -1,19 +1,25 @@
 PARTS="$@"
 if [ -z "$PARTS" ]; then
     PARTS="app"
+elif [ "$PARTS" == "-h" ] || [ "$PARTS" == "--help" ]; then
+   echo -e "Script for flashing via an esp-link device and esptool.py\nUsage:"
+   echo -e "$0 [app|bootldr|otadata|ptable|recovery]"
+   exit 1
 fi
 
 ADDR_FILES=
 for part in $PARTS
 do
     if [ "$part" == "app" ]; then
-        ADDR_FILES+="0x120000 ./build/netplayer.bin "
+        ADDR_FILES+="0x110000 ./build/netplayer.bin "
     elif [ "$part" == "bootldr" ]; then
         ADDR_FILES+="0x1000 ./build/bootloader/bootloader.bin "
     elif [ "$part" == "otadata" ]; then
         ADDR_FILES+="0xd000 ./build/ota_data_initial.bin "
     elif [ "$part" == "ptable" ]; then
         ADDR_FILES+="0x8000 ./build/partitions.bin "
+    elif [ "$part" == "recovery" ]; then
+	ADDR_FILES+="0x60000 ./recovery/build/recovery.bin "
     else
        echo "Unrecognized item '$part'"
        exit 1;
