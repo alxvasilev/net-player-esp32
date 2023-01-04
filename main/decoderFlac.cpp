1f7050ff3e7c0ebb9949afcb1194f7261ae9a52c
Alexander Vassilev <av@mega.co.nz>
2023-01-05 00:10:18 +0200

    Fix clearing streamId when flac decode error

--- main/decoderFlac.cpp
+++ main/decoderFlac.cpp
@@ -84,7 +84,8 @@ AudioNode::StreamError DecoderFlac::pullData(AudioNode::DataPullReq& dpr)
     for (int i = 0; !mOutputLen && (i < 10); i++) {
         auto ok = FLAC__stream_decoder_process_single(mDecoder);
         if (!ok) {
-            dpr.clear();
+            dpr.size = 0;      // just in case
+            dpr.buf = nullptr; //
             auto err = FLAC__stream_decoder_get_state(mDecoder);
             const char* errStr = (err >= 0) ? FLAC__StreamDecoderStateString[err] : "(invalid code)";
             ESP_LOGW(TAG, "Decoder returned error %s(%d)", errStr, err);
